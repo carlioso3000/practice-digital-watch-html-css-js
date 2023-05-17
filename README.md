fc3d22860bc4aa345de2ce16024f9f8038d38536
carlioso3000 <carlioso3006@gmail.com>
2023-05-17 19:29:51 -0400

    changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # practice-digital-watch-html-css-js
 A small and simple project where I will learn to combine the three basic technologies on web development, html, css and javascript. Small and simple with the purpose of get used to work and navigate through this three technogolies at the same time in a single project.
+
+This project is made from vanilla html, css and javascript, with the main purpose of having a real first contact and getting familiarized on how to incorporate these three technologies in a real project.
--- index-watch.js
+++ index-watch.js
@@ -3,8 +3,9 @@ function currentTime() {
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    let month = date.getMonth();
-    let year = date.getFullYear();
+    //let month = date.getMonth();
+    //let year = date.getFullYear();
+    //let day = date.getDay();
     hour = (hour < 10) ? "0" + hour : hour;
     min = (min < 10) ? "0" + min : min;
     sec = (sec < 10) ? "0" + sec : sec;
@@ -12,15 +13,9 @@ function currentTime() {
     let time = hour + ":" + min + ":" + sec;
     let watch = document.querySelector("#watch");
     watch.innerHTML = time;
+
+    let dias = document.querySelectorAll(".dias li");
+    let diaActual = new Date().getDay();
+    dias[diaActual].style.color = "#780000"
 }
-/*function currentDate() {
-    let cDate = new Date ();
-    let month = cDate.getMonth();
-    let year = cDate.getFullYear();
-
-    let currentDay = month + ", " + year;
-    let date2 = document.querySelector("#dias");
-    date2.innerHTML = currentDay;
-}*/
-
 setInterval(currentTime, 1000)
